9e66421a82009f1f69bbf15ca39a2645ad3574ac
redfoometal <mysl1m9543@gmail.com>
2024-04-26 10:52:28 +0300

    delete css

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
 import Posts from "@/components/Posts";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
      <Posts/>
     </main>
   );
